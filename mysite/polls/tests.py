89f09318838b43b1de9c2e776012b5dc8c02d4c1
CK <chris.keith@gmail.com>
2023-07-17 10:06:25 -0700

    Rough out test, not working yet

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+from pyparticleio.ParticleCloud import ParticleCloud, _ParticleDevice
+from unittest.mock import MagicMock
+from dotenv import load_dotenv
+import os
 
-# Create your tests here.
+from polls.views import index, LightSensor
+
+class TestLightSensor(TestCase):
+    def test_elapsed_time(self):
+        mockDevice = _ParticleDevice()
+        mockDevice.name = "FakePhoton"
+        
+        load_dotenv("./.env")
+        mockCloud = ParticleCloud(username_or_access_token=os.getenv("ACCESS_TOKEN"))
+        mockCloud._get_devices = MagicMock(
+            return_value = [
+                mockDevice
+            ]
+        )
+
+        lightSensor = LightSensor(mockCloud, "FakePhoton", 'Light sensor')
+        data1 = type('', (object,), {
+            "data": 'false',
+            "ttl" : 1,
+            "published_at": '2023-07-17T16:38:01.560Z',
+            "coreid" : '32002e000e47363433353735',
+            "event_name": 'Light sensor',
+        })()
+        lightSensor.handle_call_back(data1)
+        ret = index(None)
+        print(ret._container)
