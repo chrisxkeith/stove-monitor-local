7943fa919235b0529ab06802ef0b85f738db1c6b
CK <chris.keith@gmail.com>
2023-07-17 16:26:50 -0700

    Test does not error out, but does not work

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -4,29 +4,25 @@ from unittest.mock import MagicMock
 from dotenv import load_dotenv
 import os
 
-from polls.views import index, LightSensor
+from polls.views import index, history, LightSensor, lightSensor
 
 class TestLightSensor(TestCase):
     def test_elapsed_time(self):
         mockDevice = _ParticleDevice()
-        mockDevice.name = "FakePhoton"
+        mockDevice.name = "photon-07"
         
         load_dotenv("./.env")
         mockCloud = ParticleCloud(username_or_access_token=os.getenv("ACCESS_TOKEN"))
-        mockCloud._get_devices = MagicMock(
-            return_value = [
-                mockDevice
-            ]
-        )
-
-        lightSensor = LightSensor(mockCloud, "FakePhoton", 'Light sensor')
-        data1 = type('', (object,), {
-            "data": 'false',
+        lightSensor = LightSensor(mockCloud, mockDevice.name, "Light sensor")
+        data1 = {
+            "data" : "false",
             "ttl" : 1,
-            "published_at": '2023-07-17T16:38:01.560Z',
-            "coreid" : '32002e000e47363433353735',
-            "event_name": 'Light sensor',
-        })()
+            "published_at" : "2023-07-17T16:38:01.560Z",
+            "coreid" : "fake_core_id",
+            "event_name" : "Light sensor",
+        }
         lightSensor.handle_call_back(data1)
         ret = index(None)
+        print(ret._container)
+        ret = history(None)
         print(ret._container)
