10f2c06d1c5fa3a4e8a3cc9b9d27f60d01657490
CK <chris.keith@gmail.com>
2023-07-18 08:48:03 -0700

    Strip test back

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -1,19 +1,9 @@
 from django.test import TestCase
-from pyparticleio.ParticleCloud import ParticleCloud, _ParticleDevice
-from unittest.mock import MagicMock
-from dotenv import load_dotenv
-import os
 
-from polls.views import index, history, LightSensor, lightSensor
+from polls.views import App
 
 class TestLightSensor(TestCase):
     def test_elapsed_time(self):
-        mockDevice = _ParticleDevice()
-        mockDevice.name = "photon-07"
-        
-        load_dotenv("./.env")
-        mockCloud = ParticleCloud(username_or_access_token=os.getenv("ACCESS_TOKEN"))
-        lightSensor = LightSensor(mockCloud, mockDevice.name, "Light sensor")
         data1 = {
             "data" : "false",
             "ttl" : 1,
@@ -21,8 +11,3 @@ class TestLightSensor(TestCase):
             "coreid" : "fake_core_id",
             "event_name" : "Light sensor",
         }
-        lightSensor.handle_call_back(data1)
-        ret = index(None)
-        print(ret._container)
-        ret = history(None)
-        print(ret._container)
