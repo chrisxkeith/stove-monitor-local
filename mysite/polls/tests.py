66dd253a2e0d9cacfaaf15d182b3f609917001bb
CK <chris.keith@gmail.com>
2023-07-18 19:44:08 -0700

    Revive test. Still need to turn off real events

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -1,9 +1,10 @@
 from django.test import TestCase
 
-from polls.views import App
+from polls.views import index, history, LightEventHandler
 
 class TestLightSensor(TestCase):
     def test_elapsed_time(self):
+        lightEventHandler = LightEventHandler()
         data1 = {
             "data" : "false",
             "ttl" : 1,
@@ -11,3 +12,8 @@ class TestLightSensor(TestCase):
             "coreid" : "fake_core_id",
             "event_name" : "Light sensor",
         }
+        lightEventHandler.handle_call_back(data1)
+        # ret = index(None)
+        # print(ret._container)
+        ret = history(None)
+        print(str(ret._container).replace("<br>", "\n"))
