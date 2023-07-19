06c9fc32d89880b593a8a55f69e1b8b19f3a0888
CK <chris.keith@gmail.com>
2023-07-19 08:15:34 -0700

    first runs and passes

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -1,19 +1,25 @@
 from django.test import TestCase
 
-from polls.views import index, history, LightEventHandler
+from polls.views import app
 
 class TestLightSensor(TestCase):
+    def do_test(self, data, expected):
+        app.lightSensor.handle_call_back(data)
+        ret = app.handleRequest()
+        self.assertContains(ret, expected)
+
     def test_elapsed_time(self):
-        lightEventHandler = LightEventHandler()
-        data1 = {
+        self.do_test({
             "data" : "false",
             "ttl" : 1,
             "published_at" : "2023-07-17T16:38:01.560Z",
             "coreid" : "fake_core_id",
             "event_name" : "Light sensor",
-        }
-        lightEventHandler.handle_call_back(data1)
-        # ret = index(None)
-        # print(ret._container)
-        ret = history(None)
-        print(str(ret._container).replace("<br>", "\n"))
+        }, "Off")
+        # self.do_test({
+        #    "data" : "true",
+        #    "ttl" : 1,
+        #    "published_at" : "2023-07-17T16:39:01.560Z",
+        #    "coreid" : "fake_core_id",
+        #    "event_name" : "Light sensor",
+        # }, "On")
