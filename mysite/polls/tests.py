7501d7bdc68ad735fc1c3d92466d47d3cd3cc0d5
CK <chris.keith@gmail.com>
2023-08-19 08:19:45 -0700

    Set up test event time zone correctly. Expand expected values

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from datetime import datetime
+from zoneinfo import ZoneInfo
 
 from polls.views import app
 
@@ -7,34 +8,14 @@ class TestLightSensor(TestCase):
     def do_test(self, data, expected):
         app.lightSensor.handle_call_back(data)
         ret = app.handleRequest()
-        # print(ret.content)
         for e in expected:
+            if (not e in str(ret.content)):
+                 print(ret.content)
             self.assertContains(ret, e)
 
     def test_elapsed_time(self):
-        self.do_test({
-            "data" : "false",
-            "ttl" : 1,
-            "published_at" : "2023-07-17T16:38:01.560Z",
-            "coreid" : "1c002c001147343438323536",
-            "event_name" : "Light sensor",
-        }, [ "Off" ])
-        self.do_test({
-            "data" : "true",
-            "ttl" : 1,
-            "published_at" : "2023-07-17T16:39:01.560Z",
-            "coreid" : "1c002c001147343438323536",
-            "event_name" : "Light sensor",
-        }, [ "On" ])
-        self.do_test({
-            "data" : "true",
-            "ttl" : 1,
-            "published_at" : "2023-07-17T17:39:01.560Z",
-            "coreid" : "1c002c001147343438323536",
-            "event_name" : "Light sensor",
-        }, [ "Tomato" ])
         now = datetime.now()
-        more_than_an_hour_ago = datetime.fromtimestamp(now.timestamp() - (60 * 65))
+        more_than_an_hour_ago = datetime.fromtimestamp(now.timestamp() - (60 * 65)).astimezone(ZoneInfo("UTC"))
         self.do_test({
             "data" : "false",
             "ttl" : 1,
@@ -42,12 +23,12 @@ class TestLightSensor(TestCase):
             "coreid" : "1c002c001147343438323536",
             "event_name" : "Light sensor",
         }, [ "Off" ])
-        hour_ago = datetime.fromtimestamp(now.timestamp() - (60 * 60))
+        hour_ago = datetime.fromtimestamp(now.timestamp() - (60 * 60)).astimezone(ZoneInfo("UTC"))
         self.do_test({
             "data" : "true",
             "ttl" : 1,
             "published_at" : hour_ago.strftime("%Y-%m-%dT%H:%M:%S.%fZ"),
             "coreid" : "1c002c001147343438323536",
             "event_name" : "Light sensor",
-        }, [ "On" ]) # "00:00"
+        }, [ "On", "Tomato", "60:00" ])
 
